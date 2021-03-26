6beb77f370a72d79106ed12f7ba28c5fd2f5dd77
Adriano Garafulić <adrianogarafulic@gmail.com>
2021-03-26 16:49:18 +0100

    šminkanje koda, postavljanje vrijednosti za slanje na po ure

--- RX_TX_nodse_mcu/RX_TX_nodse_mcu.ino
+++ RX_TX_nodse_mcu/RX_TX_nodse_mcu.ino
@@ -1,5 +1,5 @@
 #define BLYNK_PRINT Serial
-  #include <ESP8266WiFi.h>
+ #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <SoftwareSerial.h>
 // #include <SimpleTimer.h>
@@ -17,18 +17,6 @@ String myString; // complete message from arduino, which consistors of snesors d
 char rdata; // received charactors
  
 float prva,druga,treca;
-// This function sends Arduino's up time every second to Virtual Pin (1).
-// In the app, Widget's reading frequency should be set to PUSH. This means
-// that you define how often to send data to Blynk App.
-void myTimerEvent()
-{
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V1, millis() / 1000);
-  
-}
- 
- 
  
 void setup()
 {
@@ -36,101 +24,63 @@ void setup()
   Serial.begin(9600);
  
   Blynk.begin(auth, ssid, pass);
- 
-     timer.setInterval(1000L,sensorvalue1); 
-     timer.setInterval(1000L,sensorvalue2);
-     timer.setInterval(1000L,sensorvalue3); 
+  timer.setInterval(900000L,sensorvalue1); //svaku 15 min
+  timer.setInterval(1000L,sensorvalue2);
+  timer.setInterval(1000L,sensorvalue3); 
 
  
 }
  
 void loop()
 {
-
-  
-   if (Serial.available() == 0 ) 
+if (Serial.available() == 0 ) 
    {
-  Blynk.run();
-  timer.run(); // Initiates BlynkTimer
+    Blynk.run();
+    timer.run(); // Initiates BlynkTimer
    }
    
-  if (Serial.available() > 0 ) 
+if (Serial.available() > 0 ) 
   {
     rdata = Serial.read(); 
     myString = myString+ rdata; 
-   // Serial.print(rdata);
+    // Serial.print(rdata);
     if( rdata == '\n')
-    {
-   //  Serial.println(myString); 
-  // Serial.println("fahad");
-// new code
+      {
+        String l = getValue(myString, ',', 0);
+        String m = getValue(myString, ',', 1);
+        String n = getValue(myString, ',', 2); 
 
-
-String l = getValue(myString, ',', 0);
-String m = getValue(myString, ',', 1);
-String n = getValue(myString, ',', 2); 
-
- 
- 
- prva = l.toFloat();
-druga = m.toInt();
-treca = n.toInt();
-
- 
-  myString = "";
-// end new code
-    }
-  
-/*else
- {
- String l ="0" ;
-String m = "0";
-String n = "0"; 
-String k = "0";
-String j = "0"; 
-String o = "0"; 
-String p = "0";  
- prva = 0;
-druga = 0;
-treca = 0;
-cetvrta=0;
-peta=0;
-sesta=0;
-sedma=0;
- }*/ 
-  }
+        prva = l.toFloat();
+        druga = m.toInt();
+        treca = n.toInt();
+        myString = "";
+        // end new code
+      }
+   }
 }
  
 void sensorvalue1()
 {
-float sdata = prva;
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2, sdata);
-   //Serial.print("CH4   ");
-   //Serial.println(sdata);
- 
+ float sdata = prva;
+ // You can send any value at any time.
+ // Please don't send more that 10 values per second.
+ Blynk.virtualWrite(V2, sdata);
 }
 void sensorvalue2()
 {
-int sdata = druga;
+  int sdata = druga;
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V3, sdata);
-  //Serial.print("CO   ");
-    // Serial.println(sdata);
- 
+  Blynk.virtualWrite(V3, sdata); 
 }
-
 void sensorvalue3()
 {
-int sdata = treca ;
+  int sdata = treca ;
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
   Blynk.virtualWrite(V4, sdata);
   //Serial.print("LPG   ");
-    // Serial.println(sdata);
- 
+   // Serial.println(sdata);
 }
 
 
